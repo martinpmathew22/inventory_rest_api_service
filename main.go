707bec9a7ee687fd8c6ac35cb7f092e645916a35
Martin <169165879+martinpmathew22@users.noreply.github.com>
2025-03-10 20:32:21 +0530

    Update main.go

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "4", Title: "Martin", Artist: "Martin P Mathew", Price: 99.99},
 }
 
 func main() {
